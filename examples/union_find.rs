8f53c93243db1ce34f5e4112f4937ab30bbfb9a9
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-08-25 20:36:32 +0900

    union_find: add example

---
+++ examples/union_find.rs
@@ -0,0 +1,30 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+use proconio::input;
+
+fn main() {
+    input! {
+        vertices_len: usize,
+        queries_len: usize,
+    }
+    let mut uf = cprlib::union_find::UnionFind::new(vertices_len);
+
+    input! {
+        queries: [(u64, usize, usize); queries_len],
+    }
+
+    for &(is_same, x, y) in queries.iter() {
+        if is_same == 1 {
+            println!(
+                "{}",
+                match uf.same(x, y) {
+                    true => "1",
+                    false => "No",
+                }
+            );
+            
+        } else {
+            uf.unite(x, y);
+        }
+    }
+}
