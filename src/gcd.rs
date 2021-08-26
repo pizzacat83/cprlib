2ee6f3b567cc6bbf6ce6ba6a182c052c43a25b53
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-08-27 01:26:25 +0900

    gcd: take unsigned primitive int as arguments

--- src/gcd.rs
+++ src/gcd.rs
@@ -1,16 +1,23 @@
-pub fn calc_gcd(a: u64, b: u64) -> u64 {
+use num::{Unsigned, PrimInt};
+pub trait UPrimInt: Unsigned + PrimInt {}
+impl<T> UPrimInt for T where T: Unsigned + PrimInt {}
+
+pub fn calc_gcd<U: UPrimInt>(a: U, b: U) -> U {
     let (a, b) = if a < b { (a, b) } else { (b, a) };
     calc_gcd_rec(a, b)
 }
 
-fn calc_gcd_rec(a: u64, b: u64) -> u64 {
+fn calc_gcd_rec<U: UPrimInt>(a: U, b: U) -> U {
     // a < b を仮定
-    if a == 0 {
+    if a == U::zero() {
         return b;
     }
     calc_gcd_rec(b % a, a)
 }
 
-pub fn calc_gcd_multi<Iter: Iterator<Item = u64>>(iter: Iter) -> u64 {
-    iter.fold(0, calc_gcd)
+pub fn calc_gcd_multi<U, Iter>(iter: Iter) -> U
+    where U: UPrimInt,
+    Iter: Iterator<Item = U>,
+{
+    iter.fold(U::zero(), calc_gcd)
 }
